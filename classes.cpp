66f2904bdb7ea1ffe572d8ff599da857e0fafb4a
TheEliteD <dhristov06@gmail.com>
2024-08-16 03:01:42 +0300

    Made the snake

--- classes.cpp
+++ classes.cpp
@@ -1,5 +1,19 @@
 #include <raylib.h>
 #include <globals.h>
+#include <deque>
+
+class Snake {
+    public:
+        std::deque<Vector2> body = {Vector2{6, 9}, Vector2{5, 9}, Vector2{4, 9}};
+
+        void Draw(){
+            for (unsigned int i = 0; i < body.size(); i++){
+                float x = body[i].x;
+                float y = body[i].y;
+                DrawRectangleRounded(Rectangle{x * cellSize, y * cellSize, (float)cellSize, (float)cellSize}, 0.5, 6, darkGreen);
+            }
+        }
+};
 
 class Food {
     public:
--- main.cpp
+++ main.cpp
@@ -7,11 +7,13 @@ int main(){
     SetTargetFPS(60);
 
     Food food;
+    Snake snake;
 
     while (WindowShouldClose() == false){
         BeginDrawing();
         ClearBackground(lightGreen);
 
+        snake.Draw();
         food.Draw();
 
         EndDrawing();
